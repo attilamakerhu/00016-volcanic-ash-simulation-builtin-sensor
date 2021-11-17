7ca37980b47f722a959b719411f43ffde7256a08
attilamakerhu <87415155+attilamakerhu@users.noreply.github.com>
2021-11-17 22:25:44 +0100

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
+input.onButtonPressed(Button.A, function () {
+    limit += limitStep
+})
+input.onButtonPressed(Button.AB, function () {
+    measure = false
+    basic.clearScreen()
+    basic.showNumber(limit)
+    basic.pause(500)
+    measure = true
+})
+input.onButtonPressed(Button.B, function () {
+    limit += -1 * limitStep
+})
+let measure = false
+let limitStep = 0
+let limit = 0
+limit = 250
+limitStep = 5
+measure = true
 basic.forever(function () {
-	
+    if (measure) {
+        led.plotBarGraph(
+        input.lightLevel(),
+        limit
+        )
+    }
 })
